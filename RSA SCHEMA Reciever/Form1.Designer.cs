9c17d43bc60c635450ae304625913bcc97404f76
KatinasPukis <65959494+KatinasPukis@users.noreply.github.com>
2021-06-10 10:21:38 +0300

    Initial commit

---
+++ RSA SCHEMA Reciever/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RSA_SCHEMA_Reciever
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartReceiver = new System.Windows.Forms.Button();
+            this.ReceivedTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CheckSignuture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartReceiver
+            // 
+            this.StartReceiver.Location = new System.Drawing.Point(55, 26);
+            this.StartReceiver.Name = "StartReceiver";
+            this.StartReceiver.Size = new System.Drawing.Size(113, 23);
+            this.StartReceiver.TabIndex = 0;
+            this.StartReceiver.Text = "Start Receiving";
+            this.StartReceiver.UseVisualStyleBackColor = true;
+            this.StartReceiver.Click += new System.EventHandler(this.StartReceiver_Click);
+            // 
+            // ReceivedTextBox
+            // 
+            this.ReceivedTextBox.Location = new System.Drawing.Point(55, 93);
+            this.ReceivedTextBox.Name = "ReceivedTextBox";
+            this.ReceivedTextBox.Size = new System.Drawing.Size(226, 38);
+            this.ReceivedTextBox.TabIndex = 1;
+            this.ReceivedTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Received:";
+            // 
+            // CheckSignuture
+            // 
+            this.CheckSignuture.Location = new System.Drawing.Point(55, 150);
+            this.CheckSignuture.Name = "CheckSignuture";
+            this.CheckSignuture.Size = new System.Drawing.Size(100, 23);
+            this.CheckSignuture.TabIndex = 3;
+            this.CheckSignuture.Text = "Check Signuture";
+            this.CheckSignuture.UseVisualStyleBackColor = true;
+            this.CheckSignuture.Click += new System.EventHandler(this.CheckSignuture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckSignuture);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ReceivedTextBox);
+            this.Controls.Add(this.StartReceiver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartReceiver;
+        private System.Windows.Forms.RichTextBox ReceivedTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CheckSignuture;
+    }
+}
+
---
+++ RSA SCHEMA Reciever/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA_SCHEMA_Reciever
+{
+    public partial class Form1 : Form, Iupdatable
+    {
+
+
+        private Receiver receiver = null;
+        public string folderpath = @"TempData\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartReceiver_Click(object sender, EventArgs e)
+        {
+            if (receiver == null)
+            {
+                MessageBox.Show("Recieving!");
+                Thread receiverThread = new Thread(() =>
+                {
+                    receiver = new Receiver(Environment.ExpandEnvironmentVariables(folderpath), this);
+                    receiver.Start();
+                });
+                receiverThread.Start();
+
+
+            }
+        }
+        public void displayText(string text)
+        {
+            Invoke((Action)delegate
+            {
+                //ReceivedTextBox.Text = this.MyProperty;
+                ReceivedTextBox.Text = text;
+                //PathTextBox.Text = path;
+            });
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckSignuture_Click(object sender, EventArgs e)
+        {
+            
+            ASCIIEncoding ByteConverter = new ASCIIEncoding(); // bitu converteris
+            string message = ReceivedTextBox.Text; // zinute kuri yra gauta
+            string[] text = message.Split(',');
+            string originalData = " ";
+            string Signuture = " ";
+            int counter = 0;
+            foreach (var sub in text)
+            {
+                counter++;
+                if(counter==1)
+                {
+                    originalData = sub;
+                }
+                if(counter==2)
+                {
+                    Signuture = sub;
+                }
+                
+            }
+            byte[] receivedData = ByteConverter.GetBytes(originalData);
+            byte[] signedData = Convert.FromBase64String(Signuture);
+
+            RSADigitalKey rSADigital = new RSADigitalKey();
+            rSADigital.VerifyData(receivedData, signedData);
+
+
+
+        }
+    }
+}
